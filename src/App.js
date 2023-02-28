37ea58ca8be6b503eca81cd32c4049626fbe801b
rookid19 <randyodoom19@gmail.com>
2023-02-28 11:52:35 +0800

    game description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import All5Group20 from './games/All5Group20';
 
 function App() {
   return (
     <div className="App">
-      gfdg
+     <All5Group20 />
     </div>
   );
 }
---
+++ src/utils/Data.js
@@ -0,0 +1,44 @@
+export const gamesDescription = {
+  all5Group30GameDescription: {
+    name: "All 5 Group 30",
+    rowName: ["One Pair", "One Number"],
+    rules:
+      "How to play: Select 2 One Pair & 1 One No. form 1bet. Winning No. include One No., One Pair occur 2x, as win. Win Sample：Bet plan: One Pair: 2, 8, One No.: 0, Winning No.: 2, 8, 0, 8, 2, as win.",
+  },
+
+  all5Group60GameDescription: {
+    name: "All 5 Group 60",
+    rowName: ["One Pair", "One Number"],
+    rules:
+      "How to play: Select 1 One Pair & 3 One No. form 1bet. Winning No. include One No., One Pair occur 2x, as win. Win Sample：Bet plan: One Pair: 1, One No.: 2, 3, 4, Winning No.: 1, 2, 3, 4, 1, as win.",
+  },
+
+  all5Group120GameDescription: {
+    name: "All 5 Group 120",
+    rowName: "",
+    rules:
+      "How to play: Pick 5 numbers to form 1 bet, the picked numbers match the winning number (in any order) to win. Win Sample：Bet plan: 0, 2, 3, 8, 9, Winning No.: 9, 8, 0, 2, 3, as win.",
+  },
+
+  all5StraightJoint: {
+    name: "All 5 Straight(Joint)",
+    rowName: ["1st", "2nd", "3rd", "4th", "5th"],
+    rules:
+      "How to play: Select 1 No. from 1st, 2nd, 3rd, 4th, 5th form 1bet. Selected and winning No. same & in order, as win. Win Sample：Bet plan: 1, 2, 3, 4, 5, Winning No.: 1, 2, 3, 4, 5, as win.",
+  },
+
+  all5StraightCombo: {
+    name: "All 5 Straight(Combo)",
+    rowName: ["1st", "2nd", "3rd", "4th", "5th"],
+    rules:
+      "How to play: Select 1 No. from 1st, 2nd, 3rd, 4th, 5th form all 1~5 combo. Total 5bets. Selected 5th and winning No. same, as win 5th Fixed Place, Selected 4th, 5th and winning No. same, as win 5th Fixed Place, Last2 Straight & so on. Maximum 5 prizes at one time. Win Sample：Bet plan: 1, 2, 3, 4, 5, Winning No.: 1, 2, 3, 4, 5, as win -, -, -, -, 5/-, -, -, 4, 5/-, -, 3, 4, 5/-, 2, 3, 4, 5/1, 2, 3, 4, 5, Each 1 bet.",
+  },
+
+  all5Group20: {
+    name: "All 5 Group 20",
+    rowName: ["Three of a Kind", "One No."],
+    rules:
+      "How to play: Select 1 Three of a Kind & 2 One No. form 1bet. Selected numbers occur 3x in winning No., as win. Win Sample：Bet plan: Three of a Kind: 8, One No.: 0, 2, Winning No.: 0, 8, 2, 8, 8, as win.",
+  },
+};
+
