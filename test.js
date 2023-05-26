29114c9fc7db33122b44fc7cda2edc1bb88d0e00
rookid19 <randyodoom19@gmail.com>
2023-05-26 17:29:14 +0000

    dsa

--- test.js
+++ test.js
@@ -1,18 +1,45 @@
-function generateSequence(num) {
-  let sequence = [];
+// let array = {
+//   firstArray: [1],
+//   secondArray: [1],
+//   thirdArray: [1],
+//   fourthArray: [1],
+//   fifthArray: [1],
+// };
 
-  for (let i = 1; i < 5; i++) {
-    sequence.push(i, i);
-  }
-  if (num === 9) {
-    return 5;
-  } else if (num < 9) {
-    return sequence[num - 1];
-  } else {
-    return sequence[8 - (num - 9)];
-  }
-}
+// // console.log(Object.values(arr)[0].length);
 
-// Usage example
-const generatedSequence = generateSequence(15);
-console.log(generatedSequence);
+// // let a = Object.values(arr)[0].length;
+
+// let b = Object.values(array).every(
+//   (arr) => !arr.includes(1) 
+// );
+
+// console.log(b);
+
+// // if one of the rows is empty then it means i can add more balls
+// // else clear\
+
+
+// const str = 'Hello';
+// const charArray = Array.from(str);
+// const numberArray = Array.from({ length: 15 }, (_, index) => index + 1);
+// // console.log(numberArray); // Output: [1, 2, 3, 4, 5]
+
+
+// const originalArray = [1, 2, 3, 4, 5];
+// const copyArray = Array.from(originalArray);
+// originalArray.push(6)
+// console.log(copyArray); // Output: [1, 2, 3, 4, 5]
+
+
+// const arr = ["1", "2", "3", "4", "3", "7"]
+
+const tasks = [
+  { id: 1, name: 'Task 1', completed: false },
+  { id: 2, name: 'Task 2', completed: true },
+  { id: 3, name: 'Task 3', completed: true },
+  { id: 4, name: 'Task 4', completed: true },
+  { id: 5, name: 'Task 5', completed: false }
+];
+
+
